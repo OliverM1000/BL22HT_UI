48698b1b76cdb0a41d45f959bf52837bdcdbdb9d
Oliver Mueller <oliver.mueller.1000@hotmail.com>
2024-05-29 22:33:20 -0700

    build main grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Foo</Button>;
+  return (
+    <Grid
+      templateAreas={`"header header" "nav main" "nav aux"`}
+      h="200px"
+      gap="1"
+      gridTemplateRows={"50px 1fr 1fr"}
+      gridTemplateColumns={"150px 1fr"}
+    >
+      <GridItem bg="gray.100" area={"header"}>
+        Header
+      </GridItem>
+      <GridItem bg="gray.100" area={"nav"}>
+        Navigation
+      </GridItem>
+      <GridItem bg="gray.100" area={"main"}>
+        Main
+      </GridItem>
+      <GridItem bg="gray.100" area={"aux"}>
+        Aux
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
