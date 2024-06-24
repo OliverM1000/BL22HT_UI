1903f267182106e0b060d672d8caebdfd80d8553
Oliver Mueller <oliver.mueller.1000@hotmail.com>
2024-06-23 22:28:40 -0700

    changed baseURL

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://192.168.1.205:3000/api",
+  //baseURL: "http://192.168.1.205:3000/api",
+  baseURL: "https://bl22ht-api.onrender.com/api",
 });
